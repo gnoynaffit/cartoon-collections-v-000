ca935020f1150a134375cebfcf9ff8e92b8b41cb
Tiffany <ong.tifffany1503@gmail.com>
2019-05-20 20:43:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,14 @@ def summon_captain_planet(veggies)# code an argument here
   end# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
+  answer = false
+  long_planeteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+  answer# Your code here
 end
 
 def find_the_cheese# code an argument here
